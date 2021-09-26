caf9edcb9c5e4a5ae6adcdb9b47737f3799d871c
jisungbin <ji@sungb.in>
2021-09-26 18:22:00 +0900

    Update

--- artdirector/src/main/java/io/github/jisungbin/artdirector/ArtDirector.kt
+++ artdirector/src/main/java/io/github/jisungbin/artdirector/ArtDirector.kt
@@ -2,18 +2,29 @@ package io.github.jisungbin.artdirector
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -46,16 +57,33 @@ fun ArtDirector(
                     )
                 } else { // MediaType.Video
                     val thumbnail = media.getVideoThumbnail(context)
+                    val shape = RoundedCornerShape(2.dp)
+
                     Box(modifier = Modifier.aspectRatio(1f), contentAlignment = Alignment.Center) {
                         Image(
                             bitmap = thumbnail.asImageBitmap(),
                             contentDescription = null,
                             contentScale = ContentScale.Crop
                         )
-                        Image(
-                            painter = painterResource(R.drawable.ic_round_play_arrow_24),
-                            contentDescription = null
-                        )
+                        Row(
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .clip(shape)
+                                .background(color = Color.White, shape = shape)
+                                .border(width = 1.dp, color = Color.LightGray, shape = shape)
+                                .padding(end = 4.dp, top = 2.dp, bottom = 2.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(R.drawable.ic_round_play_arrow_24),
+                                contentDescription = null,
+                                colorFilter = ColorFilter.tint(Color.LightGray)
+                            )
+                            Text(
+                                text = media.duration.toString(),
+                                modifier = Modifier.padding(start = 2.dp),
+                                color = Color.LightGray
+                            )
+                        }
                     }
                 }
             }
